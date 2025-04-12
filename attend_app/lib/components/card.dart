85cdc6158edd41dfe3990bab8882c98a0215ce3c
OmarShaher <omarshaher2011@gmail.com>
2025-04-12 21:51:00 +0200

    added the attend button

--- attend_app/lib/components/card.dart
+++ attend_app/lib/components/card.dart
@@ -1,3 +1,4 @@
+import 'package:attend_app/components/buttons.dart';
 import 'package:flutter/material.dart';
 
 // ignore: must_be_immutable
@@ -6,6 +7,7 @@ class MyCard extends StatelessWidget {
   final int sessionsLeft;
   final int daysLeft;
   final void Function()? onTap;
+  final void Function()? onDecrementSession;
 
   MyCard({
     super.key,
@@ -13,6 +15,7 @@ class MyCard extends StatelessWidget {
     required this.sessionsLeft,
     required this.daysLeft,
     required this.onTap,
+    required this.onDecrementSession,
   });
 
   @override
@@ -20,7 +23,6 @@ class MyCard extends StatelessWidget {
     return GestureDetector(
       onTap: onTap,
       child: Container(
-        
         decoration: BoxDecoration(
           color: Color.fromARGB(255, 50, 49, 49),
           borderRadius: BorderRadius.circular(12), // Rounded edges for the card
@@ -34,53 +36,53 @@ class MyCard extends StatelessWidget {
         ),
         padding: EdgeInsets.all(20),
         margin: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Text(
-              name,
-              style: TextStyle(
-                color: Color.fromARGB(255, 255, 255, 255),
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 10),
-            Row(
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Icon(
-                  Icons.add_box, // Use an appropriate icon
-                  color: Colors.white70,
-                  size: 18,
-                ),
-                SizedBox(width: 8),
                 Text(
-                  "$sessionsLeft sessions left",
+                  name,
                   style: TextStyle(
-                    color: Colors.white70,
-                    fontSize: 16,
+                    color: Color.fromARGB(255, 255, 255, 255),
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
                   ),
+                ),
+                SizedBox(height: 10),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.add_box, // Use an appropriate icon
+                      color: Colors.white70,
+                      size: 18,
+                    ),
+                    SizedBox(width: 8),
+                    Text(
+                      "$sessionsLeft sessions left",
+                      style: TextStyle(color: Colors.white70, fontSize: 16),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.calendar_today, // Use an appropriate icon
+                      color: Colors.white70,
+                      size: 18,
+                    ),
+                    SizedBox(width: 8),
+                    Text(
+                      "$daysLeft days left",
+                      style: TextStyle(color: Colors.white70, fontSize: 16),
+                    ),
+                  ],
                 ),
               ],
             ),
-            SizedBox(height: 10),
-            Row(
-              children: [
-                Icon(
-                  Icons.calendar_today, // Use an appropriate icon
-                  color: Colors.white70,
-                  size: 18,
-                ),
-                SizedBox(width: 8),
-                Text(
-                  "$daysLeft days left",
-                  style: TextStyle(
-                    color: Colors.white70,
-                    fontSize: 16,
-                  ),
-                ),
-              ],
-            ),
+            MyButton(text: 'Attend', onTap: onDecrementSession),
           ],
         ),
       ),
--- attend_app/lib/pages/Users_list.dart
+++ attend_app/lib/pages/Users_list.dart
@@ -9,17 +9,46 @@ class UsersList extends StatefulWidget {
 }
 
 class _UsersListState extends State<UsersList> {
+  // Sample data - this would typically come from a database
+  List<Map<String, dynamic>> users = [
+    {"name": "amr", "sessionsLeft": 8, "daysLeft": 8},
+    {"name": "omar", "sessionsLeft": 9, "daysLeft": 20},
+  ];
+
+  void decrementSession(int index) {
+    setState(() {
+      if (users[index]["sessionsLeft"] > 0) {
+        users[index]["sessionsLeft"]--;
+      }
+    });
+
+    // Show a confirmation message
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text("Attendance recorded for ${users[index]["name"]}"),
+        backgroundColor: Colors.green,
+        duration: Duration(seconds: 2),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Users List"),
-      ),
-      body: ListView(
-        children: [
-          MyCard(name: "amr", sessionsLeft: 8, daysLeft: 8, onTap: (){}),
-          MyCard(name: "omar", sessionsLeft: 9, daysLeft: 20, onTap: (){}),
-        ]
+      appBar: AppBar(title: Text("Users List")),
+      body: ListView.builder(
+        itemCount: users.length,
+        itemBuilder: (context, index) {
+          return MyCard(
+            name: users[index]["name"],
+            sessionsLeft: users[index]["sessionsLeft"],
+            daysLeft: users[index]["daysLeft"],
+            onTap: () {
+              // Navigation to user details or other action
+            },
+            onDecrementSession: () => decrementSession(index),
+          );
+        },
       ),
     );
   }
