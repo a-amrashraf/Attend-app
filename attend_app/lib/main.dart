2bfcd44e5b89e60b80bd2cc79c165560f7a2d62b
OmarShaher <omarshaher2011@gmail.com>
2025-04-14 16:11:52 +0200

    fixing the firebase issue

--- attend_app/lib/main.dart
+++ attend_app/lib/main.dart
@@ -4,14 +4,15 @@ import 'package:attend_app/pages/home_page.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-// import 'firebase_options.dart';
-
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp(
-  //   options: DefaultFirebaseOptions.currentPlatform, // ⬅️ This replaces the empty call
-  // );
+  await Firebase.initializeApp(
+    options:
+        DefaultFirebaseOptions
+            .currentPlatform, // ⬅️ This replaces the empty call
+  );
   runApp(const MainApp());
 }
 
@@ -24,13 +25,12 @@ void testFirebaseConnection() async {
   }
 }
 
-
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomePage(),
 
